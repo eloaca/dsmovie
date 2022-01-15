2fbb22cb2cf3a45c6b569f1e2019a174b79fa7fa
Eloa Cardozo <elooa_@hotmail.com>
2022-01-15 19:48:44 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form() {
+
+    return (
+        <h1> </h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function Listing() {
+
+    return (
+        <h1> </h1>
+    );
+}
+
+export default Listing;
